ec6c2d27af0ac1e00233473f0ff4eb7ce3c20b2b
Justin Van Dort <jtvd78@aol.com>
2018-11-13 21:32:14 -0500

    Cannot get part1 to work

--- week/10/stub.py
+++ week/10/stub.py
@@ -1,47 +1,125 @@
 #!/usr/bin/env python2
 # from the git repo
 import md5py
+import socket
+import struct
+import codecs
+
+message = 'Hello'    # original message here
+malicious = 'malicious_message'  # put your malicious message here
 
 #####################################
 ### STEP 1: Calculate forged hash ###
 #####################################
 
-message = 'Hello'    # original message here
-legit = 'a02ada15fe3e220e37ed5f139ba0cf0f'      # a legit hash of secret + message goes here, obtained from signing a message
+line_ctr = 1
+def readline(f):
+    global line_ctr
+    s = f.readline().replace('\n','')
+    print("%d\t %s" % (line_ctr, s))
+    line_ctr = line_ctr + 1
+    return s
+
+def readlines(f, num):
+    for _ in range(0, num):
+        readline(f)
+
+# Connect to server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("142.93.118.186", 1234))
+f = s.makefile()
+
+# Select 'Sign some Data'
+readlines(f, 6)
+s.send("1\n")
+
+# Send data
+readlines(f, 8)
+s.send(message+"\n")
+
+# Read generated hash
+readlines(f, 2)
+legit = readline(f).split(':')[1][1:]   # a legit hash of secret + message goes here, obtained from signing a message
+print('Hash is "%s"' % legit)
 
 # initialize hash object with state of a vulnerable hash
 fake_md5 = md5py.new('A' * 64)
 fake_md5.A, fake_md5.B, fake_md5.C, fake_md5.D = md5py._bytelist2long(legit.decode('hex'))
-
-malicious = 'malicious_message'  # put your malicious message here
 
 # update legit hash with malicious message
 fake_md5.update(malicious)
 
 # fake_hash is the hash for md5(secret + message + padding + malicious)
 fake_hash = fake_md5.hexdigest()
-print(fake_hash)
-
+print('Fake hash is "%s"' % fake_hash)
 
 #############################
 ### STEP 2: Craft payload ###
 #############################
 
-# TODO: calculate proper padding based on secret + message
-# secret is <redacted> bytes long (48 bits)
-# each block in MD5 is 512 bits long
-# secret + message is followed by bit 1 then bit 0's (i.e. \x80\x00\x00...)
-# after the 0's is a bye with message length in bits, little endian style
-# (i.e. 20 char msg = 160 bits = 0xa0 = '\xa0\x00\x00\x00\x00\x00\x00\x00\x00')
-# craft padding to align the block as MD5 would do it
-# (i.e. len(secret + message + padding) = 64 bytes = 512 bits
-padding = ''
+# Converts a 4 bit number to a hex string
+def hex4(num):
+    if num == 10:
+        return 'a'
+    if num == 11:
+        return 'b'
+    if num == 12:
+        return 'c'
+    if num == 13:
+        return 'd'
+    if num == 14:
+        return 'e'
+    if num == 15:
+        return 'f'
+    return str(num)
 
-# payload is the message that corresponds to the hash in `fake_hash`
-# server will calculate md5(secret + payload)
-#                     = md5(secret + message + padding + malicious)
-#                     = fake_hash
-payload = message + padding + malicious
+# Encode a message with \x
+def msg_encode(msg):
+    out = ''
+    for i in msg:
+        (val,) = struct.unpack('B', i)
+        out += '\\x'
+        out += hex4(val >> 4)
+        out += hex4(val & 0xF)
+    return out
 
-# send `fake_hash` and `payload` to server (manually or with sockets)
-# REMEMBER: every time you sign new data, you will regenerate a new secret!
+# Try difference secret sizes. 
+for secret_bytes in range(8, 15+1):
+   
+    # TODO: calculate proper padding based on secret + message
+    # secret is <redacted> bytes long (48 bits)
+    # each block in MD5 is 512 bits long
+    # secret + message is followed by bit 1 then bit 0's (i.e. \x80\x00\x00...)
+    # after the 0's is a bye with message length in bits, little endian style
+    # (i.e. 20 char msg = 160 bits = 0xa0 = '\xa0\x00\x00\x00\x00\x00\x00\x00\x00')
+    # craft padding to align the block as MD5 would do it
+    # (i.e. len(secret + message + padding) = 64 bytes = 512 bits
+
+    # Select 'Test a signature's validity'
+    
+    readlines(f, 6)
+    s.send("2\n")
+
+
+
+    
+
+    # Send fake hash
+    readlines(f, 3)
+    s.send('%s\n' % fake_hash)   
+
+    # Calculate padding
+    # Block Size - (Secret Size) - len(0x80) - len(message) - (8 bytes for message length)
+    zero_bytes = 64 - secret_bytes - 1 - len(message) - 8
+    padding = '\x80' + '\x00' * zero_bytes + struct.pack('<q', len(message))
+
+    # payload is the message that corresponds to the hash in `fake_hash`
+    # server will calculate md5(secret + payload)
+    #                     = md5(secret + message + padding + malicious)
+    #                     = fake_hash
+    payload = message + padding + malicious
+
+    # Sent payload. Wait for user to try next secret size. 
+    s.send(msg_encode(payload) + "\n")
+    readlines(f, 10)
+    raw_input()
